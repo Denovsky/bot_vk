b6173b45af8114a17e2346fd71b1b61393d2c02b
Denovsky <62519665+Denovsky@users.noreply.github.com>
2020-12-21 08:46:41 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -20,15 +20,11 @@ for event in longpoll.listen():
         sender = event.user_id
         attachments = []
         if reseived_message == "пикча" or reseived_message == "Картинка" or reseived_message == "картинка" or reseived_message == "Пикча":
-            random_int = random.randint(1, 2)
+            random_int = random.randint(1, 184)
             image = "img/photo0" + str(random_int) + ".jpg"
             upload_image = upload.photo_messages(photos=image)[0]
             attachments.append('photo{}_{}'.format(upload_image['owner_id'], upload_image['id']))
             write_message(sender, "Держи друк")
             print(reseived_message)
-        elif reseived_message == "дурак":
-            write_message(sender, "сам " + reseived_message)
-        elif reseived_message == "дурной ты дядя(":
-            write_message(sender, "САМ(")
         else:
             write_message(sender, "0_0 такого нету ага")
